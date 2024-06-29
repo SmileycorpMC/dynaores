e0227cf9c13cdac65576c0ca3d0d23c55c6a8758
Smileycorp <callum.j.longbottom@gmail.com>
2024-06-29 01:39:57 +0100

    re add event detection for a backup compatibility with some mods

--- src/main/java/net/smileycorp/rawores/mixin/MixinBlock.java
+++ src/main/java/net/smileycorp/rawores/mixin/MixinBlock.java
@@ -6,9 +6,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.oredict.OreDictionary;
-import net.smileycorp.rawores.common.data.OreEntry;
-import net.smileycorp.rawores.common.data.OreHandler;
+import net.smileycorp.rawores.common.CommonProxy;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,41 +21,7 @@ public class MixinBlock {
     
     @Inject(at = @At("RETURN"), method = "getDrops(Lnet/minecraft/util/NonNullList;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;I)V", remap = false)
     public void rawores$getDrops(NonNullList<ItemStack> drops, IBlockAccess access, BlockPos pos, IBlockState state, int fortune, CallbackInfo callback) {
-        try {
-            ItemStack stack = new ItemStack(state.getBlock(), 1, state.getBlock().damageDropped(state));
-            //get the first oredictionary that matches our pattern
-            String ore = getOre(stack);
-            if (ore == null) return;
-            //get the registered ore entry corresponding to the block we just broke
-            OreEntry entry = OreHandler.INSTANCE.getEntry(ore.replace("ore", ""));
-            if (entry == null) return;
-            for (int i = 0; i < drops.size(); i++) {
-                ItemStack drop = drops.get(i);
-                //check the drop is the same as the block we just broke so we don't modify extra or changed drops if other mods added them first
-                if (!ItemStack.areItemsEqual(stack, drop)) continue;
-                drops.set(i, new ItemStack(entry.getItem(), getFortune(fortune, rand)));
-            }
-        } catch (Exception e) {}
+       CommonProxy.handleDrops(state, drops, fortune, rand);
     }
-    
-    
-    private String getOre(ItemStack stack) {
-        for (int id : OreDictionary.getOreIDs(stack)) {
-            String ore = OreDictionary.getOreName(id);
-            if (ore.contains("ore")) return ore;
-        }
-        return null;
-    }
-    
-    //default to 1, might add a config to change this per ore in future
-    private int getFortune(int fortune, Random rand) {
-        return getFortune(fortune, 1, rand);
-    }
-    
-    //ore drop formula copied from diamond ore, it's the same formula used by the ore_drops loot function in modern versions
-    private int getFortune(int fortune, int base, Random rand) {
-        return (Math.max(0, rand.nextInt(fortune + 2) - 1) + 1) * base;
-    }
-    
     
 }
