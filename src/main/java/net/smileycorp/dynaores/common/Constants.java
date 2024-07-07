7b988b1e87b61daf6b9fdcafdf6509847db265b5
Smileycorp <callum.j.longbottom@gmail.com>
2024-07-07 02:12:17 +0100

    update version

--- src/main/java/net/smileycorp/dynaores/common/Constants.java
+++ src/main/java/net/smileycorp/dynaores/common/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     
     public static final String MODID = "dynaores";
     public static final String NAME = "Dynamic Raw Ores";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.2";
     public static final String DEPENDENCIES = "before:crafttweaker";
     private static final String PACKAGE = "net.smileycorp.dynaores.";
     public static final String CLIENT_PROXY = PACKAGE + "client.ClientProxy";
